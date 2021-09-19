9b8887a4b2946e763b8defa1a210894e72b0876d
telicoscience <telico@yahoo.com.br>
2021-09-18 21:52:12 -0300

    Estudo de condições simples e compostas

---
+++ ex008.js
@@ -0,0 +1,9 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel>60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO')
+
+
+}
+console.log('Dirija sempre usando um cinto de segurança')
