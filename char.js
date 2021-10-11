cd54c4753b004c82476fd021dc10e76da8fbd500
Télico Oliveira <telico@yahoo.com.br>
2021-10-11 14:44:29 -0300

    num2str - converte vetor de número em char
    Implementa a função num2str, que pega qualquer vetor com número e retorna um texto composto por caracteres.

---
+++ char.js
@@ -0,0 +1,13 @@
+const num = [111, 101, 99, 120, 68]
+const str = []
+
+
+for(var i = 0; i< num.length; i++){
+str[i] = String.fromCharCode(num[i])
+
+
+	}
+console.clear()
+console.log(str.join(''))
+
+
